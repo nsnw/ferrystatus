71154e2f4a7bf21d6f8bbca07d07e7f6c3358e0e
Andy Smith <andy@nsnw.ca>
2018-11-18 21:53:48 -0800

    WSGI changes

--- bcfdata/wsgi.py
+++ bcfdata/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for bcfdata project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bcfdata.settings')
+env = os.environ.get("ENV", "prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bcfdata.settings.{}".format(env))
 
 application = get_wsgi_application()
