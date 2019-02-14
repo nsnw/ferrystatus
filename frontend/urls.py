cc0d4b1653cbc09573aa6cc67cee31d1210d5f41
Andy Smith <andy@nsnw.ca>
2019-02-13 23:02:31 -0800

    Removed routes for about/faq/privacy

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path, re_path
 from . import views
 urlpatterns = [
-    path('about', views.about ),
-    path('faq', views.faq ),
-    path('privacy', views.privacy ),
-    re_path(r'(.*)$', views.home ),
+    re_path(r'(.*)$', views.app ),
     #re_path(r'^(all-sailings|sailings|sailings/.+)$', views.sailings ),
     #re_path(r'^(routes|routes/.+)$', views.routes ),
     #path('ferries', views.ferries ),
