41406a37661e8dcb0a379be18e82f59df98366a9
Andy Smith <andy@nsnw.ca>
2019-02-13 23:03:07 -0800

    Removed functions for about/faq/privacy

--- frontend/views/__init__.py
+++ frontend/views/__init__.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request, params):
+def app(request, params):
     return render(request, 'frontend/app.html')
-
-def about(request):
-    return render(request, 'frontend/about.html')
-
-def faq(request):
-    return render(request, 'frontend/faq.html')
-
-def privacy(request):
-    return render(request, 'frontend/privacy.html')
