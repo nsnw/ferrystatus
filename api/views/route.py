0b27a212430a9ebf95c63a4cc0070e4d10f39bd8
Andy Smith <andy@nsnw.ca>
2019-02-15 23:13:44 -0800

    Tidied up, removed unused imports.

--- api/views/route.py
+++ api/views/route.py
@@ -1,16 +1,10 @@
 import logging
-from pytz import timezone
-from django.db.models import Q
-from . import response, error
+from . import response
 from data.models import Route
-from datetime import datetime, timedelta
 
 logger = logging.getLogger(__name__)
 
 def get_all(request):
-    #tz = timezone('UTC')
-    #now = tz.localize(datetime.now())
-    #hour_ago = now - timedelta(hours=1)
     routes = [
         route.as_dict for route in Route.objects.all().order_by('route_code')
     ]
